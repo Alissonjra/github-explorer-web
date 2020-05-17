ed37c3598a1a31a4cef7445cf33c37fd624a9f7b
Alisson Junior <53801159+Alissonjra@users.noreply.github.com>
2020-05-17 12:35:38 -0300

    Commit

---
+++ src/routes/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+import Dashboard from '../pages/Dashbord';
+import Repository from '../pages/Repository';
+
+const Routes: React.FC = () => (
+  <Switch>
+    <Route path="/" exact component={Dashboard} />
+    <Route path="/repositories/:repository+" component={Repository} />
+  </Switch>
+);
+
+export default Routes;
